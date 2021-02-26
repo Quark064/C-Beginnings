118032295a7680da355fdb69bf845b9e1afaa6fb
Quark064 <wolfer473gmail.com>
2021-02-26 13:17:58 -0900

    Completed Week 7 Labs and Week 7 Homework

---
+++ Code/Labs/Week 7/ConversionFromBinary.c
@@ -0,0 +1,56 @@
+/* William Anderson - 2/26/2021 - C Homework */
+#include <stdio.h>
+
+int power(int base, int pow) {
+  int i, j = base;
+  if (pow == 0) {
+    return 1;
+  }
+  for (i = 0; i < (pow-1); i++) {
+    j *= base;
+  }
+  return j;
+}
+
+int main(int argc, char **argv) {
+  const int BUF_LEN = 128;
+  char str[BUF_LEN];
+  char c;
+  int i, binLen, n = 0, is_binary = 1;
+
+  /* Get the user to enter a string */
+  printf("Please enter a string made of 0s and 1s, finishing the entry by pressing Enter.\n");
+  for (i=0; i<BUF_LEN-1; i++) {
+    scanf("%c", &c);
+    if (c == '\n') {
+      break;
+    }
+    str[i] = c;
+  }
+  str[i] = '\0';
+
+  /* Check if the user string contains nothing but 0s and 1s */
+  i = 0;
+  while (str[i] != '\0') {
+    if ((str[i] != '0') && (str[i] != '1')) {
+      is_binary = 0;
+      break;
+    }
+    i += 1;
+  }
+  binLen = i;
+
+  /* Proceed only of the string contains only 0s and 1s */
+  if (is_binary) {
+    /* Convert the string of 0s and 1s to an integer number n */
+    for (i = 0; i < binLen; i++) {
+      n += (((str[i] == 48) ? 0 : 1) * power(2, ((binLen - i) - 1)));
+    }
+
+    printf("The binary number %s is %d in decimal.\n", str, n);
+  } else {
+    printf("The string you entered, \"%s\", contains characters other than 0 and 1.\n", str);
+  }
+
+  return 0;
+}
---
+++ Code/Labs/Week 7/ConversionToBinary.c
@@ -0,0 +1,48 @@
+/* William Anderson - 2/26/2021 - C Homework */
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  int n, k, i, l = 0;
+  char tmp;
+  char str[65];
+
+  /* Make the user enter a non-negative integer */
+  printf("Please enter a non-negative integer: ");
+  scanf("%d", &n);
+  while (n < 0) {
+    printf("Sorry, your input is incorrect.\n");
+    printf("Please enter a non-negative integer: ");
+    scanf("%d", &n);
+  }
+
+  /* Convert the integer to reversed binary: e.g. 6 gets converted to 011 */
+  if (n == 0) {
+    /* Special case for n = 0 */
+    str[0] = '0';
+    str[1] = '\0';
+  } else {
+
+    k = n;
+
+    i = 0;
+    while (k > 0) {
+      str[i] = ((k % 2) ? 49 : 48);
+      k /= 2;
+      i += 1;
+    }
+    str[i] = '\0';
+    l = i - 1;
+  }
+
+/* Revserse the string */
+  for (i = 0; i < ((l / 2) + 1); i++) {
+    tmp = str[i];
+    str[i] = str[l-i];
+    str[l-i] = tmp;
+  }
+
+  /* Display the number and the string */
+  printf("The decimal number %d is %s in binary.\n", n, str);
+
+  return 0;
+}
